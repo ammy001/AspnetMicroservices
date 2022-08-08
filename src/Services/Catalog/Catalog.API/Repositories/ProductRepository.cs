6d047840930c1f3061453627730666ff7a6c42a0
ammy001 <a.c.khairnar@gmail.com>
2022-08-08 09:33:54 +0530

    Added Discount Microservice

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -64,7 +64,7 @@ namespace Catalog.API.Repositories
         }
 
         public async Task<bool> DeleteProduct(string id)
-        {
+        {   
             FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
 
             DeleteResult deleteResult = await _context
